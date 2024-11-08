c518e5455a407c0a2547d78b445d24ced5d9c0b7
hota1024 <hotalog@hota1024.com>
2024-11-09 02:10:32 +0900

    path

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { JSONPath } from "jsonpath-plus";
 import { notFound } from "next/navigation";
 import { ProjectJson } from "project-json";
 
@@ -7,8 +8,13 @@ import { JsonView } from "./json-view";
 
 export default async function ProjectPage(props: {
   params: Promise<{ id: string }>;
+  searchParams: Promise<{
+    path?: string;
+  }>;
 }) {
   const { id } = await props.params;
+  const { path } = await props.searchParams;
+  console.log(path);
 
   if (isNaN(Number(id)) || Number(id) < 0 || Number(id) === Infinity) {
     return notFound();
@@ -17,10 +23,11 @@ export default async function ProjectPage(props: {
   const info: ProjectInfo = await fetch(
     `https://api.scratch.mit.edu/projects/${id}`
   ).then((r) => r.json());
-  console.log(info);
-  const json: ProjectJson = await fetch(
+  let json: ProjectJson = await fetch(
     `https://projects.scratch.mit.edu/${id}?token=${info.project_token}`
   ).then((r) => r.json());
+
+  json = path ? JSONPath({ path: path, json: json }) : json;
 
   return (
     <main className="min-h-screen w-full">
